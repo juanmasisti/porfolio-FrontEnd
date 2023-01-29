ca16104617b25b08e53a5ac7a819df8a470c5db0
Juan Manuel Sisti <sistijuanmanuel@gmail.com>
2023-01-29 13:41:04 -0300

    Primer diseño estático

---
+++ scripts/cards.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.chart').easyPieChart({
+      size: 175,
+      barColor: "#41c0cd ",
+      scaleLength: 0,
+      lineWidth: 25,
+      trackColor: "#D1D1D1",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
